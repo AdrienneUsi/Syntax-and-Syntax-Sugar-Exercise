ebb9ce02f969010cd953e1d7d3830e407d219841
AdrienneUsi <usiadrienne35@gmail.com>
2023-12-28 19:34:34 -0800

    finish exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -5,22 +5,22 @@
         static void Main(string[] args)
         {
             //int answer = 4;
-
-           // string response;
+            // string response;
 
             // if (answer < 9)
-            //{
-                //response = answer + " is less than nine";
-           // }
-            else 
+            // {
+            //     response = answer + " is less than nine";
+            //  }
+            //   else
+            //   {
+            //      response = answer + "greater than or equal to nine";
+            //   }
 
-            {
-                //response = answer + " is greater than or equal to nine";
-            }
 
             var answer = 4;
-            var response = (answer < 9) $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
 
+            Console.WriteLine(response);
         }
     }
 }
